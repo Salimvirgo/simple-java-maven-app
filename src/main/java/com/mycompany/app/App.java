e5cfbe4c83ffaa59b0b9dd0e47fff4fb337b5223
Salimvirgo <salimmjalloh46@gmail.com>
2025-05-13 15:12:11 +0000

    change the console message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Welcome to my first Maven build pipeline";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
